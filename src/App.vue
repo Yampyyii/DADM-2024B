e858ea81e765c0401c23307a0ac059191a331658
Brayan Pallares <150222899+Yampyyii@users.noreply.github.com>
2024-09-23 18:53:02 +0000

    aun no acabo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <script setup>
 
+const header = ref ('App lista de compras');
+const items = ref([
+  {id:'0', label:'10 bolillos'},
+  {id:'1', label:'1 lata de frijoles'},
+  {id:'2', label:'1 Chela'},
+  {id:'3', label:'1 Nutella'},
+]);
 </script>
 
 <template>
-<h1>Shopping Cart</h1>
+<h1>
+<i class="material-icons shopping-cart-icon"> local_mall</i>
+{{ header }}
+</h1>
+<ul>
+ <li v-for="item in items" :key="item.id"> 👍{{ item.label- }}</li>
+</ul>
   </template>
-<style></style>
+<style scoped>
+.shopping-cart-icon{
+font-size: 2rem;
+}
+</style>
 
