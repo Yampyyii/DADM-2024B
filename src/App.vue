5110dfcf4679843628436fbfa49bb8c760f7aa34
Brayan Pallares <150222899+Yampyyii@users.noreply.github.com>
2024-10-02 18:01:24 +0000

    🌟S11 Methods Create method to add item and clean input

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,52 @@
 import { ref } from 'vue';
 //modelo
 const header = ref ('App lista de compras');
+
+//items
+//items model
 const items = ref([
   {id:'0', label:'10 bolillos'},
   {id:'1', label:'1 lata de frijoles'},
   {id:'2', label:'1 Chela'},
   {id:'3', label:'1 Nutella'},
 ]);
-
+//item-method
+const saveItem = ()=> {
+items.value.push({id: items.value.length+1, label: newItem.value});
+//clear the input
+newItem.value="";
+};
 const newItem= ref("");
-</script>
+const newItemHighPriority = ref(false);
 
+
+</script>
+//metodos
 <template>
 <h1>
 <i class="material-icons shopping-cart-icon"> local_mall</i>
 {{ header }}
 </h1>
-<input v-model="newItem" type="text"
-placeholder="Agregar articulo"> {{ newItem }}
+<!--Agregando entradas de usuario-->
+<form
+class="add-item form"
+v-on:submit.prevent="saveItem()">
+<!--Entrada de Texto-->
+<input 
+type="text"
+placeholder="Add Item"
+v-model.trim="newItem">
+<!--Radio buttons-->
+<label><input type="checkbox" v-model="newItemHighPriority">Alta prioridad</label>
+<!--boton-->
+<button
+class="btn btn-primary">
+Salvar Articulo
+</button>
+
+</form>
+
+  <!--Lista-->
 <ul>
  <li v-for="item in items" :key="item.id"> 🍻{{ item.label }}</li>
 </ul>
